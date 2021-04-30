2e90a7b855aeb8a4b7ae1f2d829cc92a032f7b33
Igor Vinicius <igorvinnicius@gmail.com>
2021-04-30 07:10:54 +0200

    implementing sign up use case

--- src/application/use_cases/auth/signUp/signUpUseCase.js
+++ src/application/use_cases/auth/signUp/signUpUseCase.js
@@ -1,6 +1,6 @@
 const User = require('../../../../entities/user');
+const DuplicateError = require('../../../errors/duplicateError');
 const SignUpError = require('../../../errors/signUpError');
-
 
 module.exports = class SignUpUseCase {
 
@@ -11,13 +11,19 @@ module.exports = class SignUpUseCase {
 
     async execute(signUpInput){
 
+        const user = await this.usersRepository.getByEmail(signUpInput.email);        
+
+        if (user) {
+            throw new DuplicateError('User already exists.');
+        }
+
         const signUpResponse = await this.authService.signUp(signUpInput.email, signUpInput.password);
 
         if(!signUpResponse.success){
             throw new SignUpError('Sign up has been failed.');
         }
 
-        const user = new User(signUpInput.name, signUpInput.email);
-        return await this.usersRepository.add(user);
+        const newUser = new User(signUpInput.name, signUpInput.email);
+        return await this.usersRepository.add(newUser);
     }
 }
--- test/unit/auth/signupTest.js
+++ test/unit/auth/signupTest.js
@@ -11,12 +11,13 @@ describe('SignUp Use Case', () => {
     it('should return a registered user correctly', async () => {        
         
         const authService = { signUp:() => {}};
-        const usersRepository = { add:() => {}}
+        const usersRepository = { add:() => {}, getByEmail:() =>{}}
 
         const signUpInput = new SignUpInput("Jon Snow", "jon@starks.com", "123456");
 
-        const expectedUser = [ new User("Jon Snow", "jon@starks.com", "123456") ];
+        const expectedUser = [ new User("Jon Snow", "jon@starks.com") ];
 
+        sinon.stub(usersRepository, 'getByEmail').returns(null);        
         sinon.stub(usersRepository, 'add').returns(expectedUser);        
 
         sinon.stub(authService, 'signUp').returns({ success: true });
@@ -30,13 +31,40 @@ describe('SignUp Use Case', () => {
         expect.expect(expectedUser).to.be.equal(registeredUser);
     });
 
+    it('should throw an error when user already exists', async () => {
+        
+        try {                       
+
+            const authService = { signUp:() => {}};            
+            const usersRepository = {getByEmail:() =>{}}
+
+            const signUpInput = new SignUpInput("Jon Snow", "jon@starks.com", "123456");
+            sinon.stub(usersRepository, 'getByEmail').returns(new User("Jon Snow", "jon@starks.com"));                       
+
+            sinon.stub(authService, 'signUp').returns({ success: false });
+
+            const signUpUseCase = new SignUpUseCase(authService, usersRepository);       
+
+            await signUpUseCase.execute(signUpInput);            
+
+            expect.expect.fail();
+
+        }
+        catch (err) {            
+            expect.expect(err.name).to.be.equal('DuplicateError');
+            expect.expect(err.message).to.be.equal('User already exists.');
+        }        
+       
+    });
+
     it('should throw an error when signUp fails', async () => {
         
         try {                       
 
-            const authService = { signUp:() => {}};
-            const usersRepository = {};            
+            const authService = { signUp:() => {}};            
+            const usersRepository = {getByEmail:() =>{}}            
 
+            sinon.stub(usersRepository, 'getByEmail').returns(null);
             const signUpInput = new SignUpInput("Jon Snow", "jon@starks.com", "123456");                        
 
             sinon.stub(authService, 'signUp').returns({ success: false });
